e3cbbc8fc7e619b48cee3161d776bdfad2888214
tux2000 <tux2000@users.noreply.github.com>
2015-03-29 21:37:32 +0200

    …

--- moisture_test.sh
+++ moisture_test.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
 /usr/local/bin/gpio write 4 0
+#/usr/local/bin/gpio write 5 0
 sleep 0.1
 /usr/local/bin/gpio aread 1
+#/usr/local/bin/gpio write 5 1
 /usr/local/bin/gpio write 4 1
-
-/usr/local/bin/gpio write 5 0
-sleep 0.1
-/usr/local/bin/gpio aread 1
-/usr/local/bin/gpio write 5 1
